c7955768ec8b1223277310036c6fdb5f5908bdbf
xyzker <410317669@qq.com>
2025-03-10 19:51:49 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //id("java")
     id("application")
-    //id("org.barfuin.gradle.taskinfo") version "2.1.0"
+    id("org.barfuin.gradle.taskinfo") version "2.2.0"
 }
 
 group = "org.example"
